750b110fd923f633a4c23f81b5e02ae99899b672
Pradeep Kumar Nirmal <31483487+pknir@users.noreply.github.com>
2024-12-29 15:07:10 +0530

    test

---
+++ popup.js
@@ -0,0 +1,35 @@
+// $(function(){
+// var overlay = $('<div id="overlay"></div>');
+// overlay.show();
+// overlay.appendTo(document.body);
+// $('.popup-onload').show();
+// $('.close').click(function(){
+// $('.popup-onload').hide();
+// overlay.appendTo(document.body).remove();
+// return false;
+// });
+
+
+ 
+
+// $('.x').click(function(){
+// $('.popup').hide();
+// overlay.appendTo(document.body).remove();
+// return false;
+// });
+// });
+
+$(function(){
+    if (!localStorage.getItem('popupShown')) {
+        var overlay = $('<div id="overlay"></div>');
+        overlay.show();
+        overlay.appendTo(document.body);
+        $('.popup-onload').show();        
+        $('.close, .x').click(function(){
+            $('.popup-onload').hide();
+            overlay.appendTo(document.body).remove();
+            localStorage.setItem('popupShown', 'true');
+            return false;
+        });
+    }
+});
